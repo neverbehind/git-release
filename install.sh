ade31f8036343a7fceed9ab7b470b3017d5c4ed4
douglasg-teachco <60362524+douglasg-teachco@users.noreply.github.com>
2020-02-10 17:31:49 -0500

    Add bash rc file to cover the bases

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 curl https://raw.githubusercontent.com/neverbehind/git-release/master/git-release -o ~/bin/git-release
  
 chmod +x ~/bin/git-release 
- 
-touch ~/.zshrc && echo "PATH=$PATH:~/bin" >> ~/.zshrc 
- 
-source ~/.zshrc  
+
+## Add local bin path
+[ "$SHELL" = "/bin/zsh" ] && touch ~/.zshrc && echo "PATH=$PATH:~/bin" >> ~/.zshrc && source ~/.zshrc  
+[ "$SHELL" = "/bin/bash" ] && touch ~/.bashrc && echo "PATH=$PATH:~/bin" >> ~/.bashrc && source ~/.bashrc  
